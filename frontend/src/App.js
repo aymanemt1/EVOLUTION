abce1cd3916416153480d702bd75a3c32d340825
Youssef Talibi <youssef.talibi11@gmail.com>
2024-02-15 12:43:01 +0100

    First commit by youssef

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+import Evolution from './Evolution.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={Evolution} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Evolution
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Evolution
         </a>
       </header>
     </div>
