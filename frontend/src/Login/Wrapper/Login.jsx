383a7be9ab4f2da138636e297c245a24db905ea3
ibrahim stitou <stitou.brahim.stitou@gmail.com>
2024-02-17 15:42:15 +0100

    commit avec login

---
+++ frontend/src/Login/Wrapper/Login.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Login.css';
+import { FaUser, FaLock } from "react-icons/fa";
+import { Link } from "react-router-dom";
+export default function Login() {
+  return (
+    <div className='Wrapper-Parent'>
+      <div className='Wrapper'>
+      <form action="">
+        <h1>Member Access</h1>
+        <div className='input-box'>
+          <input type="text" placeholder='Username' required name='loginUsername'/>
+          <FaUser className='icon'/>
+        </div>
+        <div className='input-box'>
+          <input type="password" placeholder='Password' required name='loginPassword'/>
+          <FaLock className='icon'/>
+        </div>
+        <div className='remember-forgot'>
+          <label><input type="checkbox" name='Remember' />Remember me</label>
+          <a href="#">Forgot password?</a>
+        </div>
+        <button type='submit'>Login</button>
+        <div className="register-link">
+          <p>D'ont have an account? <Link to="/signup">Register</Link></p>
+        </div>
+      </form>
+    </div>
+    </div>
+    
+  )
+}
--- frontend/src/Router/router.jsx
+++ frontend/src/Router/router.jsx
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LandingPage from "../LandingPage/landingPage";
+import LoginPage from "../Login/LoginPage";
 export default function RouterApp(){
     return (
         <Fragment>
             <BrowserRouter>
                 <Routes>
                     <Route path='/' element={<LandingPage />} />
+                    <Route path="/login" element={<LoginPage/>}/>
                 </Routes>
             </BrowserRouter>
         </Fragment>
