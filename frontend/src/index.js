56978531d9f375725bfc6c77de0bb27326fb5663
ibrahim stitou <stitou.brahim.stitou@gmail.com>
2024-05-10 18:03:55 +0100

    after commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,24 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
 import { MenuContext, MenuProvider } from './Context/MenuContext';
+import CaloriesContext from './CaloriesCalCulator/CaloriesContext';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <MenuProvider>
+    <CaloriesContext>
     <App />
-  </MenuProvider>
-=======
-import CaloriesContext from './CaloriesCalCulator/CaloriesContext';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <CaloriesContext>
-    <App />
-  </CaloriesContext>
->>>>>>> b81111bfcdb5f23ee0cadae4f96e03dc8a2ef847
-);
-
+    </CaloriesContext>
+  </MenuProvider>)
 
 reportWebVitals();
