424e7d852dc8437813294a6b4d43b53629b26152
Youssef Talibi <youssef.talibi11@gmail.com>
2024-05-14 13:06:23 +0100

    add menu and responsive navbar for exercices components

--- frontend/src/Exercices/navbar/navbar.jsx
+++ frontend/src/Exercices/navbar/navbar.jsx
@@ -1,49 +1,149 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import './navbar.css'
+import "./navbar.css";
 
-export default function Navbar(){
+export default function Navbar() {
+  const location = useLocation();
 
-    const location = useLocation();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-    return (
-        <Fragment>
-            <div className="parentNavbar">
-                <div className="leftNavbar">
-                    <Link to='/home'>
-                        <span>
-                            <img src="/assets/logos/logoRM.png" alt="Evolution" title="Evolution"/>
-                        </span>
-                    </Link>
-                </div>
-                <div className="RightNavbar">
-                    <ul className="parentULnavbar">
-                        <li>
-                            <Link to='/overview' className="Link">
-                                <button className={location.pathname == 'overview' ? "btnNavbarExercicesOverview" : "btnNavbarExercices"}><i className='bx bxs-binoculars'></i> <span>Overview</span></button>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to='/activity' className="Link">
-                                <button className={location.pathname == 'activity' ? "btnNavbarExercicesActivity" : "btnNavbarExercices"}>
-                                    <i className='bx bx-street-view'></i> <span>Activity</span>
-                                </button>
-                            </Link>
-                        </li>
-                        <li>
-                            <div className="parentSearchBarExercices">
-                                <i className='bx bx-search-alt'></i>
-                                <input type="text" />
-                            </div>
-                        </li>
-                        <li>
-                            <button className="btnNotification">
-                                <i className='bx bxs-bell' ></i>
-                            </button>
-                        </li>
-                    </ul>
-                </div>
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+
+  console.log(menuOpen)
+
+  return (
+    <Fragment>
+      <div className="parentNavbar">
+        <div className="leftNavbar">
+          <Link to="/home">
+            <span>
+              <img
+                src="/assets/logos/logoRM.png"
+                alt="Evolution"
+                title="Evolution"
+                id="imgBrand"
+              />
+            </span>
+          </Link>
+        </div>
+        <div className="RightNavbar">
+          <ul className="parentULnavbar">
+            <li>
+              <Link to="/overview" className="Link">
+                <button
+                  className={
+                    location.pathname == "overview"
+                      ? "btnNavbarExercicesOverview"
+                      : "btnNavbarExercices"
+                  }
+                >
+                  <i className="bx bxs-binoculars"></i> <span>Overview</span>
+                </button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/activity" className="Link">
+                <button
+                  className={
+                    location.pathname == "activity"
+                      ? "btnNavbarExercicesActivity"
+                      : "btnNavbarExercices"
+                  }
+                >
+                  <i className="bx bx-street-view"></i> <span>Activity</span>
+                </button>
+              </Link>
+            </li>
+            <li>
+              <div className="parentSearchBarExercices">
+                <i className="bx bx-search-alt"></i>
+                <input type="text" />
+              </div>
+            </li>
+            <li>
+              <button className="btnNotification">
+                <i className="bx bxs-bell"></i>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="parentNavbarMenu">
+        <div className="leftMenuExercices">
+          <button onClick={toggleMenu}>
+            {menuOpen ? (
+              <i className="bx bx-x" id={menuOpen ? "closeBtnWhite" : null}></i>
+            ) : (
+              <i className="bx bx-menu"></i>
+            )}
+          </button>
+          {menuOpen && (
+            <div className="menuLinks">
+              <ul>
+                <li>
+                  <Link to="/overview" className="Link" onClick={toggleMenu}>
+                    <button
+                      className={
+                        location.pathname == "overview"
+                          ? "btnNavbarExercicesOverview"
+                          : "btnNavbarExercices"
+                      }
+                    >
+                      <i className="bx bxs-binoculars"></i>{" "}
+                      <span>Overview</span>
+                    </button>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/activity" className="Link" onClick={toggleMenu}>
+                    <button
+                      className={
+                        location.pathname == "activity"
+                          ? "btnNavbarExercicesActivity"
+                          : "btnNavbarExercices"
+                      }
+                    >
+                      <i className="bx bx-street-view"></i>{" "}
+                      <span>Activity</span>
+                    </button>
+                  </Link>
+                </li>
+              </ul>
             </div>
-        </Fragment>
-    )
+          )}
+        </div>
+        <div className="centerMenu">
+          <Link to="/home">
+            <span>
+              <img
+                src="/assets/logos/logoRM.png"
+                alt="Evolution"
+                title="Evolution"
+                id={menuOpen ? "imgBrand" : null}
+              />
+            </span>
+          </Link>
+        </div>
+        <div className="rightMenuExercices">
+          <button className="btnNotification" id={menuOpen ? "notificationMenuExercices" : null}>
+            <i className="bx bxs-bell"></i>
+          </button>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
