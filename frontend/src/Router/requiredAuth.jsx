8f9e8bccff6dacb4c4b1a027f2b5e53dfb9cac30
moutoute Aymane <Moutouteayman10@gmail.com>
2024-05-27 21:17:41 +0100

    main

--- frontend/src/Router/requiredAuth.jsx
+++
@@ -1,7 +0,0 @@
-    import { useContext, useEffect, useState } from "react";
-    import { Navigate, Outlet, } from "react-router-dom";
-    export default function RequiredAuth() {
-
-       const token = localStorage.getItem('token');
-       return token  ? <Outlet /> : <Navigate to='auth/login' /> ;
-    }
